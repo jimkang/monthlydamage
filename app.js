402874b0b73d4802fb3644c32d79e22a466ae46f
Jim Kang <jimkang@gmail.com>
2015-11-02 10:24:33 -0500

    Added copy notification.

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ var mailRow = d3.select('#mail-results');
 var mailLink = d3.select('#mail-results-link');
 var copyRow = d3.select('#copy-results');
 var copyLink = d3.select('#copy-results-link');
-var copyFieldRow = d3.select('#copy-results-field-container');
 var copyField = d3.select('#copy-results-field');
+var notificationRow = d3.select('#notification-row');
 
 var clipboard = new Clipboard('#copy-results-link');
 
@@ -26,6 +26,8 @@ var clipboard = new Clipboard('#copy-results-link');
     .on('keyup', debouncedRecalculate)
     .on('change', debouncedRecalculate)
     .each(addFormElementToDict);
+
+  copyLink.on('click', notifyOfCopy);
 
   recalculate();
 })());
@@ -100,4 +102,20 @@ function getSummaryText(formOpts, damage, lineBreak) {
     'Term (in years): ' + formOpts.termInYears + lineBreak +
     'Monthly assessment (condo fees): ' + formOpts.monthlyCondoFee +  lineBreak +
     'Yearly taxes: ' + formOpts.yearlyTaxes + lineBreak;  
+}
+
+function notifyOfCopy() {
+  notificationRow.text('Results copied to clipboard.');
+  notificationRow.class('revealed', true).class('hidden', false);
+  setTimeout(fadeNotification, 2000);
+}
+
+function fadeNotification() {
+  notificationRow.class('revealed', false).class('fade', true);
+  setTimeout(hideNotification, 1000);
+}
+
+function hideNotification() {
+  notificationRow.class('fade', false).class('hidden', true);
+  notificationRow.text('');
 }
